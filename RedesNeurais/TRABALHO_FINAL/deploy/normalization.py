77b447b9093604823dee99ea439e65a66d2f3391
Rachel <belchel01@gmail.com>
2024-09-24 17:52:38 -0300

    Pickle to normalize data

--- RedesNeurais/TRABALHO_FINAL/deploy/normalization.py
+++ RedesNeurais/TRABALHO_FINAL/deploy/normalization.py
@@ -7,25 +7,21 @@ from sklearn.discriminant_analysis import StandardScaler
 from sklearn.preprocessing import OneHotEncoder, QuantileTransformer
 from tensorflow.keras.models import model_from_json
 import pandas as pd
+import joblib
 
 # Carrega os arquivos do modelo
 def normalization_(features, data): 
 	this_folder = pathlib.Path.cwd()
 	data_folder = this_folder/"RedesNeurais"/"TRABALHO_FINAL"/"deploy"
-	df = pd.read_csv(data_folder/'original_data.csv', sep=',', decimal='.')
-	df = df[features]
-	recrutamento = [[1,2,3]]
-	ohe = OneHotEncoder(categories=recrutamento,handle_unknown='ignore')
-	ohe_df = pd.DataFrame(ohe.fit_transform(df[['RecruitmentStrategy']]).toarray(), columns=['RecruitmentStrategy_1','RecruitmentStrategy_2','RecruitmentStrategy_3'])
-	df = df.join(ohe_df)
-	df = df.drop(['RecruitmentStrategy'], axis=1)
+	
+	STANDARD_SCALER = joblib.load(data_folder/'std_scaler.pkl')
+	QT_SCALER = joblib.load(data_folder/'qt_scaler.pkl')
 
-	X = df.to_numpy() 
-	STANDARD_SCALER = StandardScaler()
-	qt = QuantileTransformer(output_distribution='normal')
-	teste_quantile = qt.fit_transform(X)
+	novo_registro_transformado = STANDARD_SCALER.transform(QT_SCALER.transform(data))
 
-	STANDARD_SCALER.fit(teste_quantile)
-	novo_registro_transformado = STANDARD_SCALER.transform(qt.transform(data))
-
+	
 	return novo_registro_transformado
+
+features = ['EducationLevel','ExperienceYears','InterviewScore',
+			'SkillScore','PersonalityScore','RecruitmentStrategy']
+normalization_(features, [[2, 4, 33, 44, 55, 1, 0 ,0]])
